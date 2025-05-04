ae81c2fde9034bb1801797a846c44b58d0216342
choggernaut <cho.ggernaut@gmail.com>
2025-05-04 11:11:00 +0900

    feat(api): remove deprecated migration scripts

--- packages/api/migrations/0004_update_transaction_card_detail_table_field.sql
+++
@@ -1,3 +0,0 @@
--- Add migration script here
-ALTER TABLE transaction_card_detail
-    ALTER COLUMN merchant_id SET NOT NULL;
--- packages/api/migrations/0005_remove_cards_table_fields.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-ALTER TABLE cards
-  DROP COLUMN created_at,
-  DROP COLUMN updated_at,
-  DROP COLUMN deleted_at;
